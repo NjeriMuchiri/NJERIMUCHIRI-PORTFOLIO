05cfe3c86d587182ba5059658b41600b30ccc4e9
NjeriMuchiri <muchirinjeri3210@gmail.com>
2024-10-03 00:01:34 +0300

    Trying to push again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,18 @@ import {logo, menu, close} from '../assets';
 
 
 const Navbar = () => {
+  const[active, setACtive] = useState("'")
   return (
-    <nav className={`${styles.paddingX}`}>
-
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <link to="/" className='flex items-center gap-2' onClick={()=>{
+          setACtive("");
+          window.scrollTo(0,0);
+        }}
+        >
+          <img src={logo} alt="logo" className='w-9 h-9' object-contain/>
+        </link>
+       </div>
     </nav>
   )
 }
